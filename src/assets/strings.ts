06622d1332e70b58795bd48c06b3045b2b74f335
Alexander Åhl <7934262+alexaahl@users.noreply.github.com>
2023-07-18 14:28:39 +0200

    Dummy texts

---
+++ src/assets/strings.ts
@@ -0,0 +1,4 @@
+export const SHORT_TEXT = 'Far far away'
+export const MEDIUM_TEXT = 'Far far away, behind the word mountains'
+export const LONG_TEXT =
+  'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large.'
